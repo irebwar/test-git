bf036fd3455c88d072716b6023d8542540614a26
Ibrahim <Ibrahim@test.com>
2025-06-19 15:05:45 +0300

    add if statment

--- function.js
+++ function.js
@@ -1,7 +1,11 @@
 // This is a simple JavaScript function
 function greet(name) {
+    if (typeof name !== 'string') 
+        return "Invalid input";
+    }
     return "Hello, " + name + "!";
+
 }
 
 // Example usage
-console.log(greet("World")); // Output: Hello, World!
+console.log(greet(11));
